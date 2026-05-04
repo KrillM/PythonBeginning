d638fc07a146d5226960e59f54be5fd694b7d7fe
krille <mks160615@gmail.com>
2026-05-04 23:22:21 +0900

    Calculator

---
+++ N02_Calculator.py
@@ -0,0 +1,20 @@
+# 2의 3승
+print(2**3)
+
+# 몫을 구하고 싶을 때 //를 사용한다.
+print(10/3)
+print(10//3)
+
+# and 또는 &
+# or 또는 |
+
+from random import *
+print(random()* 10) # 0~10간의 임의의 실수 생성
+print(int(random()* 10)) # 0~10간의 임의의 정수 생성
+
+print(randrange(1,46)) # 1부터 45까지의 임의의 값 생성
+print(randrange(1,46)) # 1부터 45까지의 임의의 값 생성
+print(randrange(1,46)) # 1부터 45까지의 임의의 값 생성
+print(randrange(1,46)) # 1부터 45까지의 임의의 값 생성
+print(randrange(1,46)) # 1부터 45까지의 임의의 값 생성
+print(randrange(1,46)) # 1부터 45까지의 임의의 값 생성
---
+++ Q01.py
@@ -0,0 +1,10 @@
+station = '사당'
+sentence = '행 열차가 들어오고 있습니다.'
+
+print(station, sentence)
+
+station = '신도림'
+print(station, sentence)
+
+station = '인천공항'
+print(station, sentence)
